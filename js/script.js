b7ed0108dad700e9a6813edccfd83ea6bca4f66e
Wojciech Klama <wojciechklamaczp@gmail.com>
2025-09-17 12:27:34 +0200

    rework of js

--- js/script.js
+++ js/script.js
@@ -1,56 +1,64 @@
 {
-    const formElement = document.querySelector(".js-form")
     const finalElement = document.querySelector(".js-final")
-
-formElement.addEventListener("input", () => { 
-
     const exchangeElement = document.querySelector(".js-exchange");
-    const amountElement = document.querySelector(".js-amount");
-    const amount = amountElement.value;
     const exchange = exchangeElement.value;
 
     const calculateAmmount = (amount, exchange) => {
-    const ratePLNEUR = 0.24;
-    const ratePLNUSD = 0.27;
-    const rateEURPLN = 4.25;
-    const rateUSDPLN = 3.65;
-    const rateUSDEUR = 0.86;
-    const rateEURUSD = 1.16;
 
+        const amountElement = document.querySelector(".js-amount");
+        const amount = +amountElement.value;
 
-    switch (amount, exchange) {
-        case "EUR-PLN":
-            return amount * rateEURPLN
-        case "PLN-EUR":
-            return amount * ratePLNEUR
-        case "PLN-USD":
-            return amount * ratePLNUSD
-        case "USD-PLN":
-            return amount * rateUSDPLN
-        case "EUR-USD":
-            return amount * rateEURUSD
-        case "USD-EUR":
-            return amount * rateUSDEUR
-    }
-    }
-    const currncySelection = (exchangeElement) => {
+        const ratePLNEUR = 0.24;
+        const ratePLNUSD = 0.27;
+        const rateEURPLN = 4.25;
+        const rateUSDPLN = 3.65;
+        const rateUSDEUR = 0.86;
+        const rateEURUSD = 1.16;
 
-    switch (exchange) {
-        case "EUR-PLN":
-            return "PLN"
-        case "PLN-EUR":
-            return "EUR"
-        case "PLN-USD":
-            return "USD"
-        case "USD-PLN":
-            return "PLN"
-        case "EUR-USD":
-            return "USD"
-        case "USD-EUR":
-            return "EUR"
-    }
+        let final;
+
+        switch (amount, final, exchange) {
+            case "EUR-PLN":
+                return amount * rateEURPLN
+            case "PLN-EUR":
+                return amount * ratePLNEUR
+            case "PLN-USD":
+                return amount * ratePLNUSD
+            case "USD-PLN":
+                return amount * rateUSDPLN
+            case "EUR-USD":
+                return amount * rateEURUSD
+            case "USD-EUR":
+                return amount * rateUSDEUR
+        }
     };
-        finalElement.innerHTML = ${amount.toFixed(2)} ${exchange}
-});
 
-}
+    const currncySelection = (exchange) => {
+
+        switch (exchange) {
+            case "EUR-PLN":
+                return "PLN"
+            case "PLN-EUR":
+                return "EUR"
+            case "PLN-USD":
+                return "USD"
+            case "USD-PLN":
+                return "PLN"
+            case "EUR-USD":
+                return "USD"
+            case "USD-EUR":
+                return "EUR"
+        }
+    };
+
+    const init = () => {
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("input",calculateAmmount,currncySelection);
+    };
+
+    init();
+
+    finalElement.innerHTML = `${final.toFixed(2)} ${exchange}`
+};
